4a5c1768793c2417db0bca168316ba44c57e3819
coutkh <80269804+coutkh@users.noreply.github.com>
2021-08-30 18:46:09 +0300

    Update SessionAttribute.java

--- src/main/java/com/cout/shop/controller/SessionAttribute.java
+++ src/main/java/com/cout/shop/controller/SessionAttribute.java
@@ -13,37 +13,6 @@ package com.cout.shop.controller;
         public static final String CATEGORY_LIST = "categoryList";
 
 
-
-        /*public static final String LAST_PAGE = "lastPage";
-
-
-
-        public static final String CURRENT_CATALOG_PRODUCTS_PAGE = "currentProductsPage";
-        public static final String CURRENT_CATALOG_PRODUCTS_PER_PAGE = "currentProductsPerPage";
-        public static final String CURRENT_PROFILE_ORDERS_PAGE = "currentProfileOrdersPage";
-        public static final String CURRENT_PROFILE_ORDERS_PER_PAGE = "currentProfileOrdersPerPage";
-        public static final String CURRENT_ADMIN_ORDERS_PAGE = "currentAdminOrdersPage";
-        public static final String CURRENT_ADMIN_ORDERS_PER_PAGE = "currentAdminOrdersPerPage";
-        public static final String CURRENT_ADMIN_PRODUCTS_PAGE = "currentAdminProductsPage";
-        public static final String CURRENT_ADMIN_PRODUCTS_PER_PAGE = "currentAdminProductsPerPage";
-
-        public static final String CURRENT_ITEMS_RANGE = "currentItemsRange";
-        public static final String TOTAL_PAGE_COUNT = "totalPageCount";
-        public static final String TOTAL_ITEM_COUNT = "totalItemCount";
-
-
-        public static final String BRAND_LIST = "brandList";
-        public static final String CURRENT_CATEGORY = "currentCategory";
-        public static final String CART_PRICE = "cartPrice";
-        public static final String CART_ITEMS = "cartItems";
-
-        public static final String CURRENT_PRODUCT = "currentProduct";
-        public static final String SUCCESSFUL_ORDER = "successfulOrder";
-        public static final String SEARCH_STRING = "searchString" ;
-        public static final String PRODUCT_FILTER_METHOD = "productFilterMethod";
-
-        public static final String CONVERTING_MULTIPLIER = "conv";*/
-
         private SessionAttribute(){
         }
     }
